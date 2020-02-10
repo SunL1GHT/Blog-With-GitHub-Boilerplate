45656f6a591004f6c559459254d941eaa55a2213
SunL1GHT <GARYGU9426@GMAIL.COM>
2020-02-10 21:29:23 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,18 +14,18 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "SunL1/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
-site_name = "人生 · 七年"
+site_name = "不好意思"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "xxxx"
 email = "hi@imalan.cn"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "最大的正义。"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
